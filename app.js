cb0ebd68a1b0b189e945aa21ba61b415aca3d132
Sorin <ionitasor@gmail.com>
2023-07-31 14:56:35 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card option
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheesburger',
+            img: 'images/cheesburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'white',
+            img: 'images/white.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheesburger',
+            img: 'images/cheesburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'white',
+            img: 'images/white.png'
+        }
+    ];
+
+    const grid = document.querySelector('.grid');
+
+    // create your board   
+    // ......................................
+    // You can create an <img> element by using the document.createElement() method
+    //   var x = document.createElement("IMG");
+    //   x.setAttribute("src", "img_pulpit.jpg");
+    //   x.setAttribute("width", "304");
+    //   x.setAttribute("height", "228");
+    //   x.setAttribute("alt", "The Pulpit Rock");
+    //   document.body.appendChild(x);
+    // ......................................
+    //  data-* attributes allow us to store extra information on standard, semantic HTML elements 
+
+
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            //card.addEventListener('click', flipcard);
+            grid.appendChild(card);
+        }
+    }
+
+    createBoard();
+
+})
